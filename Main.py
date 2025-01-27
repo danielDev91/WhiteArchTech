c61d355d4c3d08459fcb5dbcc0ec4a31062f2563
Daniel <118483762+danielDev91@users.noreply.github.com>
2025-01-27 11:00:00 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -29,10 +29,13 @@ class BoxFrame(ctk.CTkFrame):
         news_label = ctk.CTkLabel(self.news_container, text=news_text, fg_color="gray20", corner_radius=6)
         news_label.grid(row=self.news_container.grid_size()[1], column=0, padx=10, pady=(5, 5), sticky="ew")
 
-frame = BoxFrame(app, title="News", size=450)
+welcome_label = ctk.CTkLabel(app, text="Welcome to WhiteArchTech!\nThis application is base on CyberWhiteArch. \nIt's  totally privacy-focused and open-source virus and hash scanner.", fg_color="transparent", bg_color='transparent', corner_radius=6)
+welcome_label.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+welcome_label.configure(font=("Helvetica", 20, "bold"))
+app.grid_rowconfigure(0, weight=1)
+app.grid_columnconfigure(0, weight=1)
+
+frame = BoxFrame(app, title="Annoucement", size=450)
 frame.grid(row=1, column=0, padx=20, pady=20)
-frame.add_news_items("Breaking news: AI Companions getting more conversation")
-frame.add_news_items("Update : New features will be added to the application.")
-frame.add_news_items("Announcement: Upcoming interface on WhiteArchTech")
 
 app.mainloop()
