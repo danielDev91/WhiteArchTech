3663d641f5e048757fd529a1e2a33b383e344b02
Daniel <118483762+danielDev91@users.noreply.github.com>
2025-01-28 12:50:17 +0100

    Update text.py

--- text.py
+++ text.py
@@ -22,13 +22,8 @@ class Test(customtkinter.CTk):
         self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="Image Exemple", compound="left", font=customtkinter.CTkFont(size=15, weight="bold")) #image= 'something or a logo'
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
 
-        self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Home", fg_color="transparent", text_color=("white10", "white90"), hover_color=("white70", "white30"), anchor="w", command=home_button_event)
-
-        # def select_frame_by_name(self, name):
-        #     self.home_button.configure(fg_color=(""))
-
-        # def home_button_event():
-        #     pass
+        self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Home", fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), anchor="w")
+        self.home_button.grid(row=1, column=0, sticky="ew")
     
 if __name__=="__main__":
     test = Test()
